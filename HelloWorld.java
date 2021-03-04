2e98762b1ad444bce784d63b26474b2b9a754bad
AndresCarrenoP <andres.carreno@live.com>
2021-03-03 21:40:24 -0600

    let´s try the git commit/push again

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String [] args){
 		System.out.println("Hello World!! This is my text");
+		System.out.println("checking the git commit and push from console, wish me luck :)");
 	}
 }
 	
